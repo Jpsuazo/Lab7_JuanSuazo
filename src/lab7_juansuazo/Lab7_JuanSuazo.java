c1b636726d9b925aa96cbf9a7248fd5acc29dd55
Juan Suazo <jpsuazo76@gmail.com>
2026-09-04 13:49:24 -0600

    Primer commit

---
+++ src/lab7_juansuazo/Lab7_JuanSuazo.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab7_juansuazo;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author juanp
+ */
+public class Lab7_JuanSuazo {
+
+   static Scanner entrada = new Scanner(System.in);
+    public static void main(String[] args) {
+        int opcion;
+        do {
+            System.out.println("iniciar el pacman");
+            System.out.println("Volver an jugar ");
+            System.out.println("Ingrese una opcion");
+            System.out.println("Salir");
+            opcion = entrada.nextInt();
+            switch (opcion) {
+                case 1:
+                    char [][] tablero = new char [10][15];
+                    Random random = new Random();
+                    for (int i = 0; i < tablero.length; i++) {
+                        for (int j = 0; j < tablero[i].length; j++) {
+                            tablero[i][j]='*';
+                            
+                        }
+                        
+                    }
+                    int filas;
+                    int columnas;
+                    do {
+                        filas = random.nextInt(10);
+                        columnas = random.nextInt(15);
+                    } while (filas== 0 && columnas == 0);
+                    tablero[filas][columnas] = 'o';
+                    
+                    for (int i = 0; i < tablero.length; i++) {
+                        for (int j = 0; j < tablero[i].length; j++) {
+                            System.out.print(tablero[i][j]+" ");
+                            
+                        }
+                        System.out.println();
+                        
+                    }
+                    break;
+                    
+                case 2:
+                    
+                    break;
+            }
+        } while (opcion!=3);
+    }
+    
+}
